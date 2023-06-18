1c9975ad03167c359978fee96872aacceb08d9b1
Mihnea Popescu <everything.nowro@gmail.com>
2023-06-18 20:28:18 +0300

    google login

--- app/src/main/java/com/mihneapopescu/cookingrecipes/auth/LoginActivity.java
+++ app/src/main/java/com/mihneapopescu/cookingrecipes/auth/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.mihneapopescu.cookingrecipes.auth;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,11 +19,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.mihneapopescu.cookingrecipes.BuildConfig;
 import com.mihneapopescu.cookingrecipes.MainActivity;
 import com.mihneapopescu.cookingrecipes.R;
 
@@ -28,6 +42,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText etEmail;
     private EditText etPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +68,24 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 registerUser();
+            }
+        });
+
+        // Google sign in
+        String apiKey = BuildConfig.GOOGLE_SIGN_IN_API_KEY;
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(apiKey)
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        SignInButton googleSignInButton = findViewById(R.id.sign_in_button);
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                googleSignInActivityResultLauncher.launch(signInIntent);
             }
         });
     }
@@ -132,6 +165,46 @@ public class LoginActivity extends AppCompatActivity {
                 });
     }
 
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("LOGIN", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("LOGIN", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // Here, you might want to start the next activity or do anything you want with the user
+                            // Start the MainActivity
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            // Finish the LoginActivity
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("LOGIN", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+    private void handleGoogleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            firebaseAuthWithGoogle(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("LOGIN", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
     // Validation
     private boolean isValidEmail(String email) {
         return Patterns.EMAIL_ADDRESS.matcher(email).matches();
@@ -140,4 +213,18 @@ public class LoginActivity extends AppCompatActivity {
     private boolean isValidPassword(String password) {
         return password.length() >= 6;
     }
+
+    private final ActivityResultLauncher<Intent> googleSignInActivityResultLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // The Task returned from this call is always completed, no need to attach a listener
+                            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                            handleGoogleSignInResult(task);
+                        }
+                    }
+            );
+
 }
